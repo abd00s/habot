78ca62930061f596efc5768bb62c81f1f73ebda6
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-09 17:28:38 -0400

    Goals#create (#11)
    * goals#create route
    
    * goals#create action
    
    * ignore block length  in spec files
    
    * Revert "add factory_bot_rails gem"
    
    This reverts commit acce600460430191e8265fc3120aa4fcec5a42d1.
    
    * handle failures and include status codes
    
    * rebase missed these
    
    * spec post success
    
    * spec failure

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,11 +1,29 @@
 class GoalsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
   def index
     render json: goals
+  end
+
+  def create
+    if new_goal.valid?
+      render json: new_goal, status: :created
+    else
+      render json: new_goal.errors.messages, status: :bad_request
+    end
   end
 
   private
 
   def goals
     @goals ||= Goal.all
+  end
+
+  def new_goal
+    @new_goal ||= Goal.create(goal_params)
+  end
+
+  def goal_params
+    params.require(:goal).permit(:user_id, :title, :frequency, :period)
   end
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -7,4 +7,48 @@ RSpec.describe GoalsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST #create" do
+    let(:user) { create(:user) }
+    describe "with valid params" do
+      it "returns http success" do
+        post :create, params: valid_create_params
+        expect(response).to have_http_status(:created)
+      end
+
+      it "creates a Goal record" do
+        expect { post :create, params: valid_create_params }.to(
+          change { Goal.count }.by(1)
+        )
+      end
+    end
+
+    describe "with a missing params" do
+      let(:invalid_params) do
+        valid_create_params.tap { |p| p.dig(:goal).except!(:title) }
+      end
+
+      it "returns http failure" do
+        post :create, params: invalid_params
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it "creates a Goal record" do
+        expect { post :create, params: invalid_params }.to_not(
+          change { Goal.count }
+        )
+      end
+    end
+
+    def valid_create_params
+      @valid_create_params ||= {
+        goal: {
+          user_id:   user.id,
+          title:     "test goal",
+          frequency: 3,
+          period:    "week"
+        }
+      }
+    end
+  end
 end
