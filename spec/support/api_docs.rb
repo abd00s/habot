8b2bcbf5c39992e586c011d36d2bd96abafb7f9a
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-11 13:58:58 -0400

    make doc gen optional through env var toggle

--- spec/support/api_docs.rb
+++ spec/support/api_docs.rb
@@ -1,7 +1,11 @@
 require_relative "api_docs/generator.rb"
 
 module ApiDocs
+  GEN_DOCS = ENV["DOCS"].to_s == "true"
+
   def write_docs(request:, response:, options: {})
+    return unless GEN_DOCS
+
     ApiDocs::Generator.write!(
       request:  request,
       response: response,
