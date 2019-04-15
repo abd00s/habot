74519c00edfd595c36c742821a17546215829e4e
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-15 14:21:12 -0400

    beginning of period date not datetime

--- db/migrate/20190415165905_create_goal_periods.rb
+++ db/migrate/20190415165905_create_goal_periods.rb
@@ -2,7 +2,7 @@ class CreateGoalPeriods < ActiveRecord::Migration[5.2]
   def change
     create_table :goal_periods do |t|
       t.belongs_to :goal, foreign_key: true
-      t.datetime :start_date
+      t.date :start_date
       t.boolean :goal_met, default: false
 
       t.timestamps
