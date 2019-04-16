f18d2536b3e2105a42239e775d4ba1c40ac097f7
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-16 15:48:24 -0400

    validator spec made independent of particular model

--- spec/validators/beginning_of_week_validator_spec.rb
+++ spec/validators/beginning_of_week_validator_spec.rb
@@ -1,18 +1,24 @@
 require "rails_helper"
 
 RSpec.describe BeginningOfWeekValidator, type: :model do
-  let(:goal_period) { build(:goal_period) }
+  StartDateValidatable = Struct.new(:start_date) do
+    include ActiveModel::Validations
+
+    validates :start_date, beginning_of_week: true
+  end
+
+  subject { StartDateValidatable.new }
 
   let(:monday) { Time.zone.today.beginning_of_week }
 
   it "allows Monday for `start_date`" do
-    expect(goal_period).to allow_value(monday).for(:start_date)
+    should allow_value(monday).for(:start_date)
   end
 
   it "doesn't allow other days than Monday for `start_date`" do
     non_mondays = (1..6).map { |i| monday + i }
     non_mondays.each do |day|
-      expect(goal_period).not_to allow_value(day).for(:start_date)
+      should_not allow_value(day).for(:start_date)
     end
   end
 end
