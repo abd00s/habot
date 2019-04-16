c8d10a12094f39a0829cb644b8ab2374129b9fb6
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-15 20:19:29 -0400

    correct the name of the factory file

--- spec/factories/goal_period_factory.rb
+++ spec/factories/goal_period_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :goal_period do
-    goal { nil }
-    start_date { "2019-04-15 12:59:05" }
+    goal
+    start_date { "2019-04-15" }
     goal_met { false }
   end
 end
