a56a00de64d37af9be3cee7525732b2061d351a6
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-16 17:00:36 -0400

    assert invalid records with correct error

--- spec/models/goal_period_spec.rb
+++ spec/models/goal_period_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe GoalPeriod, type: :model do
     should belong_to(:goal)
     should have_many(:events)
   end
+
+  context "with an invalid start_date" do
+    let(:tuesday) { Time.zone.now.beginning_of_week + 1.day }
+    subject { create(:goal_period) }
+
+    before(:each) do
+      subject.start_date = tuesday
+    end
+
+    it "makes the record invalid" do
+      expect(subject).to_not be_valid
+    end
+
+    it "should raise invalid record with an invalid start_date" do
+      expected_message = I18n.t(
+        :week_start,
+        scope: "activerecord.errors.models.goal_period.attributes.start_date"
+      )
+      expect { subject.save! }.to raise_error ActiveRecord::RecordInvalid,
+                                              /#{expected_message}/
+    end
+  end
 end
