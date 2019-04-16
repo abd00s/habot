7931c368acfb7ee5cc278f4c21889be341dc055d
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-16 12:26:17 -0400

    remove binding

--- app/validators/beginning_of_week_validator.rb
+++ app/validators/beginning_of_week_validator.rb
@@ -1,6 +1,5 @@
 class BeginningOfWeekValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    # binding.pry
     return if value&.monday?
 
     record.errors.add(attribute, :week_start)
