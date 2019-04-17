81d6ab26853dc1ace6ed59fbf16fd39ef0a03418
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-17 13:24:39 -0400

    shoulda include the new uniq val

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Event, type: :model do
-  it "validates presence of required fields" do
+  it "runs the required validations" do
     should validate_presence_of(:goal_period_id)
     should validate_presence_of(:date)
+    should validate_uniqueness_of(:date).scoped_to(:goal_period_id)
   end
 
   it "has the correct associations" do
