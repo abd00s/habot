76de8e7a5829a8257e4227f2bd5538d1d9d23b92
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-17 12:41:15 -0400

    correct the context blocks grouping/indentation

--- spec/services/events/new_event_spec.rb
+++ spec/services/events/new_event_spec.rb
@@ -19,41 +19,41 @@ module Events
           expect(event.goal_period).to eq(event.event.goal_period)
         end
       end
-    end
 
-    context "when a goal_period exists" do
-      context "for the same period" do
-        let!(:exsisting_goal_period) do
-          create(:goal_period, goal: goal, start_date: monday)
+      context "when a goal_period exists" do
+        context "for the same period" do
+          let!(:exsisting_goal_period) do
+            create(:goal_period, goal: goal, start_date: monday)
+          end
+
+          it "creates an Event record associated to the existing GoalPeriod" do
+            event = new_event(tuesday)
+
+            expect(event.goal_period).to eq(exsisting_goal_period)
+          end
         end
 
-        it "creates an Event record associated to the existing GoalPeriod" do
-          event = new_event(tuesday)
+        context "for a past period" do
+          let!(:exsisting_goal_period) do
+            create(:goal_period, goal: goal, start_date: monday - 7.days)
+          end
 
-          expect(event.goal_period).to eq(exsisting_goal_period)
-        end
-      end
+          it "creates a new goal_period record" do
+            expect { new_event(tuesday) }.to change { GoalPeriod.count }
+              .from(1).to(2)
+          end
 
-      context "for a past period" do
-        let!(:exsisting_goal_period) do
-          create(:goal_period, goal: goal, start_date: monday - 7.days)
-        end
+          it "creates an Event record associated to the new GoalPeriod" do
+            event = new_event(tuesday)
 
-        it "creates a new goal_period record" do
-          expect { new_event(tuesday) }.to change { GoalPeriod.count }
-            .from(1).to(2)
-        end
+            expect(event.goal_period).to eq(event.event.goal_period)
+          end
 
-        it "creates an Event record associated to the new GoalPeriod" do
-          event = new_event(tuesday)
+          it "does not associate Event to the existing GoalPeriod" do
+            event = new_event(tuesday)
 
-          expect(event.goal_period).to eq(event.event.goal_period)
-        end
-
-        it "does not associate Event to the existing GoalPeriod" do
-          event = new_event(tuesday)
-
-          expect(event.goal_period).not_to eq(exsisting_goal_period)
+            expect(event.goal_period).not_to eq(exsisting_goal_period)
+          end
         end
       end
     end
