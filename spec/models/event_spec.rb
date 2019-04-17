e645d58ed0be34b714545cb728d25cb5682cec59
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-17 14:06:43 -0400

    assert record not unique error is raised

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,4 +10,31 @@ RSpec.describe Event, type: :model do
   it "has the correct associations" do
     should belong_to(:goal_period)
   end
+
+  context "in a raw SQL INSERT" do
+    let!(:event) { create(:event) }
+
+    it "does not allow duplicate values on `goal_period` and `date`" do
+      assert_raises ActiveRecord::RecordNotUnique do
+        insert_duplicate_record
+      end
+    end
+  end
+
+  def insert_duplicate_record
+    connection.execute <<-SQL
+      INSERT INTO
+        `events` (`goal_period_id`, `date`, `created_at`, `updated_at`)
+      VALUES
+        ("#{event.goal_period_id}", "#{event.date}", "#{now}", "#{now}")
+    SQL
+  end
+
+  def connection
+    @connection ||= ApplicationRecord.connection
+  end
+
+  def now
+    @now ||= Time.zone.now.to_s(:db)
+  end
 end
