4b72b3b335a1ccd37fcccf2ae354379bcda9e5b4
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-19 12:59:54 -0400

    Revert "Eliminate date ambiguity by validatign input string (#19)" (#20)
    This reverts commit ecb6f72b851ed2273ce0bd3a9b8d018266efdbb9.

--- app/services/events/new_event.rb
+++ app/services/events/new_event.rb
@@ -1,14 +1,6 @@
 module Events
   class NewEvent
-    include ActiveModel::Validations
-
-    DATE_FOMAT =
-      %r{\A(20)\d\d([-/.])(0[1-9]|1[012])\2(0[1-9]|[12][0-9]|3[01])\z}.freeze
-
-    attr_reader :goal, :date, :event, :goal_period
-
-    validates_presence_of :goal, :date
-    validates_format_of :date, with: DATE_FOMAT
+    attr_reader :event, :goal_period
 
     def self.create(args = {})
       new(args).tap(&:create)
