fc8c26ff1b93a5a6f0019ef7090a4b4048c4494d
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-19 17:34:57 -0400

    rescue from missing params error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from(ActionController::ParameterMissing) do |missing_param_exception|
+    errors = [missing_param_exception.param => "is required"]
+
+    render json: { errors: errors }, status: :bad_request
+  end
 end
