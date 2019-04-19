57cba64ff3bfb4715fca24ef8aa8720cc8a1c029
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-19 14:49:06 -0400

    create new record or rescue with error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,31 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
+    if create_event.valid?
+      render json: create_event.event, status: :created
+    else
+      render json: create_event.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def create_event
+    @create_event ||= Events::NewEvent.create(
+      goal: goal,
+      date: date
+    )
+  end
+
+  def goal
+    @goal ||= Goal.find(event_params[:goal_id])
+  end
+
+  def date
+    @date ||= Date.parse(event_params[:date])
+  end
+
+  def event_params
+    params.require(:event).permit(:goal_id, :date)
   end
 end
