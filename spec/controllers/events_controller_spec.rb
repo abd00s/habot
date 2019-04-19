c421a6973de31be58f539ecd9daf6221e0a29084
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-19 17:58:29 -0400

    spec failure on absence of params

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,28 @@
 require "rails_helper"
 
 RSpec.describe EventsController, type: :controller do
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:goal) { create(:goal) }
+
+  describe "POST #create" do
+    context "with missing params" do
+      let(:invalid_params) do
+        valid_create_params.tap { |p| p.dig(:event).except!(:date) }
+      end
+
+      it "returns http success" do
+        get :create, params: invalid_params
+
+        expect(response).to have_http_status(:bad_request)
+      end
     end
+  end
+
+  def valid_create_params
+    @valid_create_params ||= {
+      event: {
+        goal_id: goal.id,
+        date:    "2019-01-02"
+      }
+    }
   end
 end
