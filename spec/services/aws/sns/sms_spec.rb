ddbe87770b8cace2610794c85a904b249eb2b2da
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-05-04 16:01:22 -0400

    spec when missing args

--- spec/services/aws/sns/sms_spec.rb
+++ spec/services/aws/sns/sms_spec.rb
@@ -5,14 +5,42 @@ module Aws
   module Sns
     RSpec.describe Sms do
       include SnsStub
+
       before { stub_sns_client }
 
       describe ".send_single" do
-        it "publishes a message to the SNS client" do
-          client = Aws::SNS::Client.new
-          client.expects(:publish).with(payload)
+        let(:client) { Aws::SNS::Client.new }
 
-          Aws::Sns::Sms.send_single(payload)
+        context "when the required arguments are passed" do
+          it "publishes a message to the SNS client" do
+            client.expects(:publish).with(payload)
+
+            Aws::Sns::Sms.send_single(payload)
+          end
+        end
+      end
+
+      context "with missing arguments" do
+        context "both arguments missing" do
+          it "raises ArgumentError error" do
+            expect { Aws::Sns::Sms.send_single }.to raise_error(ArgumentError)
+          end
+        end
+
+        context "phone number  missing" do
+          it "raises ArgumentError error" do
+            expect do
+              Aws::Sns::Sms.send_single(payload.except(:phone_number))
+            end.to raise_error(ArgumentError)
+          end
+        end
+
+        context "message  missing" do
+          it "raises ArgumentError error" do
+            expect do
+              Aws::Sns::Sms.send_single(payload.except(:message))
+            end.to raise_error(ArgumentError)
+          end
         end
       end
 
