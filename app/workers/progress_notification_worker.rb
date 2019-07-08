d6f0af2272b4cc90fe6f0a96a3bc71d6520832c3
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-07-08 10:47:14 -0400

    deploy a worker to test it

---
+++ app/workers/progress_notification_worker.rb
@@ -0,0 +1,10 @@
+class ProgressNotificationWorker
+  include Sidekiq::Worker
+
+  def perform
+    Aws::Sns::Sms.send_single(
+      message:      "from worker",
+      phone_number: Rails.application.credentials.test_data[:phone_number]
+    )
+  end
+end
