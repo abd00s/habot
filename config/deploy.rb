5139f9f61420eba8d641b01333aea273b186f369
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-07-09 11:03:02 -0400

    Set up sidekiq (#31)
    * sidekiq gem
    
    * deploy a worker to test it
    
    * dummy service
    
    * namespace it
    
    * ping
    
    * werrker
    
    * remove dummy code
    
    * sidekiq deploy configurations
    
    * add sidekiq UI routes
    
    * sidekiq-scheduler gem
    
    * HC phonenumber for now
    
    * write a sidekiq config file
    
    * add scheduler to UI
    
    * unschedule
    
    * dynamicize phone number
    
    * remove 'schedule' heading with empty body
    
    * add config file option to prod configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,4 +34,21 @@ end
 
 after "deploy:publishing", "deploy:restart"
 
+namespace :sidekiq do
+  task :quiet do
+    on roles(:app) do
+      capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
+    end
+  end
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, :sidekiq
+    end
+  end
+end
+
+after "deploy:starting", "sidekiq:quiet"
+after "deploy:reverted", "sidekiq:restart"
+after "deploy:published", "sidekiq:restart"
+
 # rubocop:enable Metrics/LineLength
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  require "sidekiq-scheduler/web"
+  mount Sidekiq::Web => "/sidekiq"
+
   resources :goals, only: %w[index create]
   resources :events, only: :create
 end
