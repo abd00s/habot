ddafc11670819e207d7e76243a89ac38627c8901
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-07-11 14:23:13 -0400

    semantics

--- spec/workers/progress_notification_worker_spec.rb
+++ spec/workers/progress_notification_worker_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ProgressNotificationWorker, type: :worker do
-  describe ".perform_async" do
+  describe "#perform" do
     let(:goal) { create(:goal) }
 
     it "calls the goal notification manager service for the goal" do
